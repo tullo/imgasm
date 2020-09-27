95a7d91bdc595b2f9bce0a67cb67ed2dc2af4cfe
Andreas Amstutz <tullo@users.noreply.github.com>
2020-09-27 18:46:11 +0200

    configure badger options

--- db/badger.go
+++ db/badger.go
@@ -3,13 +3,17 @@ package db
 import (
 	"github.com/danskeren/database/kv"
 	"github.com/dgraph-io/badger/v2"
+	"github.com/dgraph-io/badger/v2/options"
 )
 
 var BadgerDB kv.KV
 
 func init() {
 	var err error
-	BadgerDB, err = kv.Open(badger.DefaultOptions("./badger.db"))
+	opts := badger.DefaultOptions("./badger.db")
+	opts.ValueLogLoadingMode = options.FileIO
+	opts.TableLoadingMode = options.FileIO
+	BadgerDB, err = kv.Open(opts)
 	if err != nil {
 		panic(err)
 	}
